d6416a7d115ed283d61fbe1b3e7ed5dd2d560477
Dylan Tordjman <dylantordjman@hotmail.fr>
2018-05-14 20:52:54 +0200

    first commit

--- app.js
+++ app.js
@@ -7,9 +7,16 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var session = require("express-session");
 
 var app = express();
-
+app.use(
+ session({
+  secret: 'a4f8071f-c873-4447-8ee2',
+  resave: false,
+  saveUninitialized: false,
+ })
+);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
